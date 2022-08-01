8d359d47d5229668cc9b04ee148cf3451fceba7d
mike gaunt <merkaberk91@yahoo.com>
2022-08-01 10:10:35 -0700

    add inst/ folder and files

---
+++ R/helpers_file_manipulation.R
@@ -0,0 +1,28 @@
+#' Make new R script from template.
+#'
+#' This function copies an R script template from the `gauntlet` package and writes it to a new location as defined by the user.
+#' Write-to folder locations must exist before running this code.
+#'
+#' @param folder a string name
+#' @param file_name a string name (include .r extentsion)
+#'
+#' @return a script in the location and with the name choosen by the user
+#' @export
+#'
+#' @examples
+#' #none
+make_new_script = function(folder = "code", file_name = NULL){
+  folder_location = here::here("code")
+  file_location = here::here(str_glue("{folder}/{file_name}"))
+
+  stopifnot("Please provide a filename (include .r extentsion)..." = !is.null(file_name)
+            ,"Folder location does not exist, please make it first..." = exists(folder_location))
+
+  file.copy(
+    system.file("scripts", "zz_emptyScriptTemplate_CODE.r", package="gauntlet")
+    ,file_location
+  )
+
+  file.edit(file_location)
+}
+
